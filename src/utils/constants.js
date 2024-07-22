872358471f56daccc38f9e0bd010b7c91e6a841f
umeshmahato28 <mahatoumesh94@gmail.com>
2024-07-22 20:08:35 +0530

    fixed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -12,10 +12,8 @@ export const API_OPTIONS = {
   headers: {
     'Content-Type': "application/json",
     Authorization: 
-    "Bearer" + process.env.REACT_APP_TMDB_KEY
+    "Bearer " + process.env.REACT_APP_TMDB_KEY
   },
 };
 
 export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
-
-
