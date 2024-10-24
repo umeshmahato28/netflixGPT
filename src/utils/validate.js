0586d68201ff02b6719875ddddaf5f10a9b548a7
umeshmahato28 <mahatoumesh94@gmail.com>
2024-10-24 21:11:25 +0530

    display error messege

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -3,7 +3,7 @@ export const checkValidData = (email, password) => {
       const isPasswordValid = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/.test(password)
       // const isNameValid = /^([a-zA-z,/.-]+)\s([a-zA-z,/.-]+)$/.test(name)
 if(!isEmailValid) return "Email ID is not Valid"
-if(!isPasswordValid) return "Password is not Valid"
+if(!isPasswordValid) return "Password is not Valid, Use A-Z, a-z, 0-9, Special characters(@#$%&)"
 // if(!isNameValid) return "Name is not Valid"
 
 return null
